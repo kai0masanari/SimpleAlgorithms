913fae1704dc7208bfa69657d244c889ef7a4258
kai0masanari <k-toyama@sumilab.org>
2016-10-08 15:13:18 +0900

    initial commit

---
+++ binarysearch.lisp
@@ -0,0 +1,16 @@
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+  
+(defun guess-my-number2 ()
+  (floor (/ (+ *small* *big*) 2)))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))    
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
