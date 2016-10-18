0b19993c4567521a70c62d3e260b23a36ab0123e
kai0masanari <k-toyama@sumilab.org>
2016-10-18 19:07:18 +0900

    fix fizzbuzz.lisp

--- binarysearch.lisp
+++ binarysearch.lisp
@@ -2,15 +2,15 @@
 (defparameter *big* 100)
 
 (defun guess-my-number ()
-  (ash (+ *small* *big*) -1))
+	(ash (+ *small* *big*) -1))
   
 (defun guess-my-number2 ()
-  (floor (/ (+ *small* *big*) 2)))
+	(floor (/ (+ *small* *big*) 2)))
 
 (defun smaller ()
-  (setf *big* (1- (guess-my-number)))    
-  (guess-my-number))
+	(setf *big* (1- (guess-my-number)))    
+	(guess-my-number))
 
 (defun bigger ()
-  (setf *small* (1+ (guess-my-number)))
-  (guess-my-number))
+	(setf *small* (1+ (guess-my-number)))
+	(guess-my-number))
--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -1,7 +1,9 @@
-(loop for i from 1 upto 100 do
-	(cond ((zerop (mod i 15))(print 'fizzbuzz))
-		  ((zerop (mod i 3))(print 'fizz))
-		  ((zerop (mod i 5))(print 'buzz))
-		  ((and (/= (mod i 15) 0) (/= (mod i 3) 0) (/= (mod i 5) 0)) (print i))
+(defun fizzbuzz (val)
+	(loop for i from 1 upto val do
+		(cond ((zerop (mod i 15))(print 'fizzbuzz))
+			((zerop (mod i 3))(print 'fizz))
+			((zerop (mod i 5))(print 'buzz))
+			((and (/= (mod i 15) 0) (/= (mod i 3) 0) (/= (mod i 5) 0)) (print i))
+		)
 	)
 )
